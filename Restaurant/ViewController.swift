3faa5f1302596de6e4b336ee4273761dba390f7c
saeed salim <smsalem@moci.gov.sa>
2019-01-11 16:51:37 +0300

    first commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by saeed salim on 05/05/1440 AH.
+//  Copyright © 1440 saeed salim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
